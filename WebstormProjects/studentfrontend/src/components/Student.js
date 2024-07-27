68ba1631ee520458d356c8c1fcdea74e53cb22aa
Ranindu Amarasinghe <raninduamarasinghe@gmail.com>
2024-07-28 00:14:31 +0530

    all done'

--- WebstormProjects/studentfrontend/src/components/Student.js
+++ WebstormProjects/studentfrontend/src/components/Student.js
@@ -2,17 +2,34 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import {Container, Paper,Button} from "@mui/material";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 export default function Student() {
     const paperStyle={padding:"50px 20px",width:600,margin:"20px auto"};
    const [name, setName] = useState('')
     const [address, setAddress] = useState('')
+    const [student, setStudent] = useState([])
     const handleClick=(e)=>{
        e.preventDefault()
         const student={name,address}
         console.log(student)
+        fetch("http://localhost:8080/student/add",{
+            method:"POST",
+            headers:{"Content-Type":"application/json"},
+            body:JSON.stringify(student)
+        }).then(()=>{
+           console.log("New Student Added")
+        })
     }
+
+    useEffect(() => {
+        fetch("http://localhost:8080/student/getAll")
+        .then(response => response.json())
+            .then((result)=>{
+            setStudent(result);
+        }
+            )
+    }, []);
     return (
         <Container>
             <Paper elevation={3} style={paperStyle}>
@@ -29,8 +46,19 @@ export default function Student() {
             <TextField id="standard-basic" label="Student Address " variant="standard" fullWidth value={address} onChange={(e)=>setAddress(e.target.value)} />
             <Button variant="outlined" onClick={handleClick}>Submit</Button>
         </Box>
-                {name}
-                {address}
+
+            </Paper>
+            <h1>Students</h1>
+            <Paper elevation={3} style={paperStyle}>
+                {student.map(student=>(
+                    <Paper elevation={6} style={{margin:"10px", padding:"15px", textAlign:"left"}}key={student.id}>
+Id:{student.id}
+                        Name:{student.name}
+                        Address:{student.address}
+                    </Paper>
+                    ))
+                }
+
             </Paper>
         </Container>
     );
