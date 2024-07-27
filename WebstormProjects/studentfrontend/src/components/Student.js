5a66677d8e4e472535cb08dda706b9fc15ab1979
Ranindu Amarasinghe <raninduamarasinghe@gmail.com>
2024-07-27 23:45:27 +0530

    add buttons and get data to form

--- WebstormProjects/studentfrontend/src/components/Student.js
+++ WebstormProjects/studentfrontend/src/components/Student.js
@@ -1,10 +1,18 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import {Container, Paper} from "@mui/material";
+import {Container, Paper,Button} from "@mui/material";
+import {useState} from "react";
 
 export default function Student() {
     const paperStyle={padding:"50px 20px",width:600,margin:"20px auto"};
+   const [name, setName] = useState('')
+    const [address, setAddress] = useState('')
+    const handleClick=(e)=>{
+       e.preventDefault()
+        const student={name,address}
+        console.log(student)
+    }
     return (
         <Container>
             <Paper elevation={3} style={paperStyle}>
@@ -17,9 +25,12 @@ export default function Student() {
             noValidate
             autoComplete="off"
         >
-            <TextField id="standard-basic" label="Student Name" variant="standard" fullWidth />
-            <TextField id="standard-basic" label="Student Address " variant="standard" fullWidth />
+            <TextField id="standard-basic" label="Student Name" variant="standard" fullWidth value={name} onChange={(e)=>setName(e.target.value)} />
+            <TextField id="standard-basic" label="Student Address " variant="standard" fullWidth value={address} onChange={(e)=>setAddress(e.target.value)} />
+            <Button variant="outlined" onClick={handleClick}>Submit</Button>
         </Box>
+                {name}
+                {address}
             </Paper>
         </Container>
     );
