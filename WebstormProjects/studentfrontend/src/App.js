96097992e7ef43c34ca06d13234c674b7bf96a5c
Ranindu Amarasinghe <raninduamarasinghe@gmail.com>
2024-07-27 23:12:56 +0530

    create student component

--- WebstormProjects/studentfrontend/src/App.js
+++ WebstormProjects/studentfrontend/src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
+import Appbar from "./components/Appbar";
+
+
+
 
 function App() {
   return (
     <div className="App">
-     This is ranindu.
+    <Appbar/>
     </div>
   );
 }
--- WebstormProjects/studentfrontend/src/components/Appbar.js
+++ WebstormProjects/studentfrontend/src/components/Appbar.js
@@ -3,11 +3,11 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 
-export default function ButtonAppBar() {
+export default function Appbar() {
     return (
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static">
@@ -22,9 +22,9 @@ export default function ButtonAppBar() {
                         <MenuIcon />
                     </IconButton>
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        News
+                       StudentSys
                     </Typography>
-                    <Button color="inherit">Login</Button>
+
                 </Toolbar>
             </AppBar>
         </Box>
